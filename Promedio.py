8c9f7dc74d91270d551a813d8556aacd4d1c2841
TorresOlveraGael <gaeltorreso23.cb128@dgeti.sems.gob.mx>
2024-09-23 20:32:52 -0600

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,15 @@
+#En esta actvidad realizaremos un codigo que nos diga la cantidad de gasolina gastada
+
+print (" ") 
+print ("Torres Olvera Gael") 
+print (" ")
+
+num1 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+num2 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+num3 = int(input("Introduce un numero: ")) #Nos pide introducir un valor
+
+promedio = (num1+num2+num3) / 3 #Indica la operacion a realizar
+
+print ("El promedio de los numeros es: ", promedio) #El resultado final
